1c222d1c6a1de710c6ba7f774b94be621e98d7e9
lizan <lizansarkar16@gmail.com>
2025-11-23 14:05:50 +0600

    delete data from my parcel component

--- src/pages/dashboard/myPercels/MyPercels.jsx
+++ src/pages/dashboard/myPercels/MyPercels.jsx
@@ -2,19 +2,51 @@ import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import UseAuth from "../../../hooks/UseAuth";
 import useAxiosSicure from "../../../hooks/useAxiosSicure";
-import { TbEdit } from "react-icons/tb";
+import { TbEdit, TbEyeBolt, TbTrash } from "react-icons/tb";
+import Swal from "sweetalert2";
 
 export default function MyPercels() {
   const { user } = UseAuth();
   const axiosSicure = useAxiosSicure();
 
-  const { data: parcels = [] } = useQuery({
+  const { data: parcels = [], refetch } = useQuery({
     queryKey: ["myParcels", user?.email],
     queryFn: async () => {
       const res = await axiosSicure.get(`/parcels?email=${user.email}`);
       return res.data;
     },
   });
+
+  // parcel delete function for percel delete
+  const handleDelteParcel = (id) => {
+    console.log(id);
+
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSicure.delete(`/parcels/${id}`).then((res) => {
+          console.log("from delete data function", res.data);
+
+          if (res.data.deletedCount) {
+            Swal.fire({
+              title: "Deleted!",
+              text: "Your file has been deleted.",
+              icon: "success",
+            });
+            //refresh the data on time in the ui
+            refetch();
+          }
+        });
+      }
+    });
+  };
   return (
     <div>
       <h1>All My Percels : {parcels.length}</h1>
@@ -35,17 +67,28 @@ export default function MyPercels() {
             <tbody>
               {/* row 1 */}
               {parcels.map((parcel, index) => {
-                return <tr key={parcel._id}>
-                  <th>{index + 1}</th>
-                  <td>{parcel.parcelName}</td>
-                  <td>{parcel.cost}</td>
-                  <td>{parcel.createdAt}</td>
-                  <td>
-                    <button className="btn btn-square hover:bg-primary">
-                      <TbEdit></TbEdit>
-                    </button>
-                  </td>
-                </tr>
+                return (
+                  <tr key={parcel._id}>
+                    <th>{index + 1}</th>
+                    <td>{parcel.parcelName}</td>
+                    <td>{parcel.cost}</td>
+                    <td>{parcel.createdAt}</td>
+                    <td>
+                      <button className="btn btn-square hover:bg-primary">
+                        <TbEyeBolt></TbEyeBolt>
+                      </button>
+                      <button className="btn btn-square hover:bg-primary mx-2">
+                        <TbEdit></TbEdit>
+                      </button>
+                      <button
+                        onClick={() => handleDelteParcel(parcel._id)}
+                        className="btn btn-square hover:bg-primary"
+                      >
+                        <TbTrash></TbTrash>
+                      </button>
+                    </td>
+                  </tr>
+                );
               })}
             </tbody>
           </table>
